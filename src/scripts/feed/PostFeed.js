3fa50708fb4e8cffc8032e8c43ce8ac505015636
DArmstrong87 <lenzflare@gmail.com>
2021-08-20 17:00:57 -0500

    User can now click on the star to like a post and the like is stored in the database. Clicking the star again deletes the like and renders the empty star

--- src/scripts/feed/PostFeed.js
+++ src/scripts/feed/PostFeed.js
@@ -1,40 +1,73 @@
-import { applicationElement, deletePost, getCurrentUser, getPosts, getUsers } from "../data/provider.js"
+import { createLike, deleteLike, deletePost, getCurrentUser, getLikes, getPosts, getUsers } from "../data/provider.js"
 
-applicationElement.addEventListener("click",
+document.addEventListener("click",
     click => {
         const clicked = click.target.id
         if (clicked.startsWith('deletePost--') === true) {
             const [, postId] = clicked.split("--")
-            console.log('button clicked')
             deletePost(postId)
+        }
+    }
+)
+document.addEventListener("click",
+    click => {
+        const clicked = click.target.id
+        if (clicked.startsWith('deleteLike--') === true) {
+            const [, likeId] = clicked.split("--")
+            deleteLike(parseInt(likeId))
+        }
+    }
+)
+document.addEventListener("click",
+    click => {
+        const clicked = click.target.id
+        if (clicked.startsWith('like--') === true) {
+            const [, postId] = clicked.split("--")
+            const currentUser = getCurrentUser()
+            const likeDataToAPI = {
+                userId: currentUser,
+                postId: parseInt(postId)
+            }
+            createLike(likeDataToAPI)
         }
     }
 )
 
 const listPosts = (post) => {
-    console.log(post)
     const users = getUsers()
     const currentUser = getCurrentUser()
+    const likes = getLikes()
     const date = new Date(post.timestamp)
     const foundUser = users.find(
         user => {
             return user.id === post.userId
         })
+    const foundLike = likes.find(
+        like => {
+            return like.postId === post.id && like.userId === currentUser
+        }
+    )
     let html =
         `<div class="post">
-        <h3>${post.title}</h3>
-        <img class="post__image" src=${post.imageUrl}>
-        </div>
-        <div class="post">
-        Posted by <b><a href="">${foundUser.name}</a></b> on ${date}
-        </div>
-        <div class="post__actions">
-        <img class="post__icon" src="./images/favorite-star-blank.svg" />`
+            <h3>${post.title}</h3>
+            <img class="post__image" src=${post.imageUrl}>
+            </div>
+            <div class="post__tagline">${post.description}</div>
+            <div class="post">
+            Posted by <b><a href="">${foundUser.name}</a></b> on ${date}
+            </div>
+            <div class="post__actions">`
+
+    if (foundLike) {
+        html += `<img class="post__icon" id="deleteLike--${foundLike.id}" src="./images/favorite-star-yellow.svg" />`
+    } else {
+        html += `<img class="post__icon" id="like--${post.id}" src="./images/favorite-star-blank.svg" />`
+    }
 
     if (post.userId === currentUser) {
-        html += `<img class="post__icon" id="deletePost--${post.id}" src="./images/block.svg" />`
+        html += `<img class="post__icon" id="deletePost--${post.id}" src="./images/block.svg" /></div>`
     }
-    html += `</div></div>`
+    html += `</div>`
     return html
 }
 
