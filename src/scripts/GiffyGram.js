6fdab66488730776cfa1dd68dbcc5378a22c223b
Josh Pulley <josh.pulley@icloud.com>
2021-08-20 09:25:04 -0500

    upadted main js and giffygram js

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,5 +1,7 @@
 export const GiffyGram = () => {
 
-    // Show main main UI
-    return `<h1>Giffygram</h1>`
+    
+    return `<div>NavBar</div>
+            <div>PostFeed</div>
+            <div>Footer</div>`
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,16 @@
 import { GiffyGram } from "./GiffyGram.js"
-
+import { LoginForm } from "./auth/Login.js"
 const applicationElement = document.querySelector(".giffygram")
 
 export const renderApp = () => {
     const user = parseInt(localStorage.getItem("gg_user"))
-
+    //fetch calls here, once fetch are complete then check if user
     if (user) {
         applicationElement.innerHTML = GiffyGram()
     } else {
         applicationElement.innerHTML = LoginForm()
     }
 }
+renderApp()
+document.addEventListener("stateChanged",
+    (event)=> renderApp())
