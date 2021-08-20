5d5048bfbd17a8ed7016c8790cb4639e88b1b07e
Josh Pulley <josh.pulley@icloud.com>
2021-08-20 09:59:54 -0500

    refactored fetch calls.then

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,10 +7,10 @@ export const renderApp = () => {
     const user = parseInt(localStorage.getItem("gg_user"))
     //fetch calls here, once fetch are complete then check if user
     fetchUsers()
-    fetchFollows()
-    fetchLikes()
-    fetchMessages()
-    fetchPosts()
+    .then(fetchFollows)
+    .then(fetchLikes)
+    .then(fetchMessages)
+    .then(fetchPosts)
     .then (()=>{
         if (user) {
             applicationElement.innerHTML = GiffyGram()
