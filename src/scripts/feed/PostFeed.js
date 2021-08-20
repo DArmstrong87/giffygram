b266bf75110e551c8dfd9eb6dac5193d2cfe7226
DArmstrong87 <lenzflare@gmail.com>
2021-08-20 15:43:42 -0500

    displaying all icons. There is a conditional that checks if the post userId matches the currentUser and renders a trash icon if true

--- src/scripts/feed/PostFeed.js
+++ src/scripts/feed/PostFeed.js
@@ -1,15 +1,31 @@
-import { getPosts } from "../data/provider.js"
+import { getCurrentUser, getPosts, getUsers } from "../data/provider.js"
 
 
 const listPosts = (post) => {
     console.log(post)
-    return `<div class="post">
-        <h3>
-            ${post.title}
-        </h3>
+    const users = getUsers()
+    const currentUser = getCurrentUser()
+    const date = new Date(post.timestamp)
+    const foundUser = users.find(
+        user => {
+            return user.id === post.userId
+        })
+    let html =
+        `<div class="post">
+        <h3>${post.title}</h3>
         <img class="post__image" src=${post.imageUrl}>
         </div>
-        `
+        <div class="post">
+        Posted by <b><a href="">${foundUser.name}</a></b> on ${date}
+        </div>
+        <div class="post__actions">
+        <img class="post__icon" src="./images/favorite-star-blank.svg" />`
+
+    if (post.userId === currentUser) {
+        html += `<img class="post__icon" src="./images/block.svg" />`
+    }
+    html += `</div></div>`
+    return html
 }
 
 export const postFeed = () => {
