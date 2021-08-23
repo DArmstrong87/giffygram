a1f1b074a6592e81dd50e02728ee6b78edd2aa56
Travis Hall <t.hall89d@gmail.com>
2021-08-23 12:39:29 -0400

    created createDirectMessage and setRecipientUser fns in provider.js. Created event listeners to setRecipientUser id when selecting user from drop down. Created event listener to send message and update API

--- src/scripts/message/MessageForm.js
+++ src/scripts/message/MessageForm.js
@@ -1,14 +1,51 @@
 import {
+  createDirectMessage,
   getDisplayMessageForm,
   getUsers,
+  getCurrentUser,
   setDisplayMessageForm,
+  setRecipientUser,
 } from "../data/provider.js";
 const applicationElement = document.querySelector(".giffygram");
+let selectedRecipient = "";
 
 document.addEventListener("click", (click) => {
   if (click.target.id === "directMessage__cancel") {
     setDisplayMessageForm(false);
     applicationElement.dispatchEvent(new CustomEvent("stateChanged"));
+  }
+});
+document.addEventListener("change", (event) => {
+  if (event.target.id === "select--recipient") {
+    const [, recipientId] = event.target.value.split("--");
+    selectedRecipient = parseInt(recipientId);
+    console.log(selectedRecipient);
+    setRecipientUser(selectedRecipient);
+    applicationElement.dispatchEvent(new CustomEvent("stateChanged"));
+  }
+});
+
+document.addEventListener("click", (click) => {
+  if (click.target.id === "directMessage__submit") {
+    const currentUser = getCurrentUser();
+    const userMessage = document.querySelector("input[name='message']").value;
+    const recipientUser = selectedRecipient;
+
+    const dataToAPI = {
+      userId: currentUser,
+      recipientId: recipientUser,
+      text: userMessage,
+      read: false,
+    };
+    if (dataToAPI.text === "" || dataToAPI.recipientId === "") {
+      window.alert("Please fill in all fields");
+    } else {
+      window.alert("Message sent");
+      createDirectMessage(dataToAPI);
+      setDisplayMessageForm(false);
+      selectedRecipient = "";
+      console.log(selectedRecipient);
+    }
   }
 });
 
@@ -18,11 +55,15 @@ export const MessageForm = () => {
     <div class="directMessage" id="directMessageForm">
     <h3>Direct Message</h3>
     <div>Recipient:
-    <select name="directMessage__userSelect" class="message__input">
+    <select name="directMessage__userSelect" class="message__input" id="select--recipient">
     <option>Choose a recipient...</option>
     ${users
       .map((user) => {
-        return `<option id="users--${user.id}" value="recipient--${user.id}">${user.name}</option>`;
+        if (selectedRecipient === user.id) {
+          return `<option selected value="recipient--${user.id}">${user.name}</option>`;
+        } else {
+          return `<option value="recipient--${user.id}">${user.name}</option>`;
+        }
       })
       .join("")}
     </select>
